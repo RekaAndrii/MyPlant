080bf04e7cf970d9c1747bf66f18d9d0ccf1622d
RekaAndrii <RekaAndrii@gmail.com>
2017-04-18 19:32:35 +0300

    Init commit

---
+++ src/main/java/com/my/plant/MyPlantApplication.java
@@ -0,0 +1,12 @@
+package com.my.plant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPlantApplication.class, args);
+	}
+}
---
+++ src/test/java/com/my/plant/MyPlantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.my.plant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyPlantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
