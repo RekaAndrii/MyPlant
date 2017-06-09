b696512e30bc195395c6108d5a82aa8f658fbdcf
RekaAndrii <RekaAndrii@gmail.com>
2017-06-09 20:58:33 +0300

    Change direction of sorting

--- src/main/java/com/my/plant/util/comparator/BlockComparator.java
+++ src/main/java/com/my/plant/util/comparator/BlockComparator.java
@@ -1,14 +1,14 @@
 package com.my.plant.util.comparator;
 
-import java.util.Comparator;
-
 import com.my.plant.model.Block;
+
+import java.util.Comparator;
 
 public class BlockComparator implements Comparator<Block>{
     
     @Override
     public int compare(Block o1, Block o2) {
         // Older first
-        return o2.getLastExecution().compareTo(o1.getLastExecution());
+        return o1.getLastExecution().compareTo(o2.getLastExecution());
     }
 }
