0d1d511814b53ee0dd1e0b3396562448de5cfb6a
RekaAndrii <RekaAndrii@gmail.com>
2017-06-30 22:08:47 +0300

    Fix NullPointer in Corparator

--- src/main/java/com/my/plant/util/comparator/BlockComparator.java
+++ src/main/java/com/my/plant/util/comparator/BlockComparator.java
@@ -2,13 +2,30 @@ package com.my.plant.util.comparator;
 
 import com.my.plant.model.Block;
 
+import java.time.LocalDate;
 import java.util.Comparator;
 
 public class BlockComparator implements Comparator<Block>{
-    
+    private final int PLUS = 1;
+    private final int MINUS = -1;
+    private final int EQUAL = 0;
+
+
     @Override
     public int compare(Block o1, Block o2) {
+        LocalDate date1 = o1.getLastExecution();
+        LocalDate date2 = o2.getLastExecution();
         // Older first
+        if(date1 == null && date2 == null){
+            return EQUAL;
+        }else{
+            if(date1 == null){
+                return MINUS;
+            }
+            if(date2 == null){
+                return PLUS;
+            }
+        }
         return o1.getLastExecution().compareTo(o2.getLastExecution());
     }
 }
